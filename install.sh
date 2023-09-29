aeb2ec84e4b0395464c98de89dae6a7192b22c2f
Billy Sudarsono <billysudarsono@Billy-Sudarsono.local>
2023-09-29 13:21:34 +0700

    tekitcom v1

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ pip install django
 pip install django-crispy-forms
 pip install crispy-bootstrap4
 
-python3 tekitcom/manage.py runserver 0.0.0.0:8080
+python3 tekitcom/manage.py runserver 0.0.0.0:8080 --insecure
